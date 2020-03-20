956e31579ddaf4e33f3d42fb99fdb9a9549bebd0
Kurilo Denis <webprogram2017@mail.ru>
2020-03-20 21:23:16 +0300

    api city

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 const formSearch = document.querySelector('.form-search'),
 	  inputCitiesFrom = document.querySelector('.input__cities-from'),
 	  dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
@@ -5,11 +6,16 @@ const formSearch = document.querySelector('.form-search'),
 	  dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
 	  inputDateDepart = document.querySelector('.input__date-depart');
 
-const city = ['Москва', 'Санкт-Петербург', 'Минск', 'Караганда', 'Челябинск', 'Керчь', 'Волгоград', 'Самара', 'Днепропетровск', 'Киев', 'Екатеринбург', 'Одесса', 'Ухань', 'Шымкент', 'Нижний Новгород'
-, 'Калининград', 'Вроцлав', 'Ростов на Дону'];
+//Данные 
+const citiesApi = '../dataBase/cities.json',
+			proxy = 'https://cors-anywhere.herokuapp.com/',
+			API_KEY = 'e3884d4f1381cd8f3ab95ee0bbe80c29',
+			calendar = 'http://min-prices.aviasales.ru/calendar_preload';
+
+let city = [];
 
 //Функция API получение городов
-const getData = (url) => {
+const getData = (url, callback) => {
 	//объект запроса
 	const request = new XMLHttpRequest();
 	request.open('GET', url);
@@ -18,32 +24,27 @@ const getData = (url) => {
 		if(request.readyState !== 4) return;
 
 		if(request.status === 200){
-			console.log(request);
+			callback(request.response);
 		}else{
 			console.error(request.status);
 		}
 	});
-
 	request.send();
-};
-
-getData('https://jsonplaceholder.typicode.com/todos/1');
+}
 
 const showCity = (input, list) => {
-	list.textContent = '';
+	list.textContent = ''; // очищаем выпадающее меню
 
 	if(input.value !== ''){ 
-		
 		const filterСity = city.filter((item) => {
-			const fixItem = item.toLowerCase();
-
-			return fixItem.includes(input.value.toLowerCase());
+			const fixItem = item.name.toLowerCase();
+			return fixItem.includes(input.value.toLowerCase());	
 		})
 		filterСity.forEach((item) => {
 			const li = document.createElement('li');
 			li.classList.add('dropdown__city');
-			li.textContent = item;
-			list.append(li)
+			li.textContent = item.name;
+			list.append(li);
 		});
 	}
 };
@@ -66,14 +67,24 @@ inputCitiesTo.addEventListener('input', () => {
 
 
 //Выбираем город по клику
-dropdownCitiesFrom.addEventListener('click', () => {
+dropdownCitiesFrom.addEventListener('click', (event) => {
 	selectCity(event, inputCitiesFrom, dropdownCitiesFrom);
 });
 
-dropdownCitiesTo.addEventListener('click', () => {
+dropdownCitiesTo.addEventListener('click', (event) => {
 	selectCity(event, inputCitiesTo, dropdownCitiesTo);
 });
 
+//Вызовы функций
+getData(citiesApi, (data) => {
+	city = JSON.parse(data).filter((item) => {
+		return item.name;
+	});
+});
+
+getData(proxy + calendar + '?depart_date=2020-05-25&origin=SVX&destination=KGD&one_way=true&token=' + e3884d4f1381cd8f3ab95ee0bbe80c29, (data) 
+	const cheapTicket = JSON.parse(data).best_prices.filter(item => item.depart_date === '2020-05-25')
+	console.log(cheapTicket);
 
 
 
